9295c58b09972d6275928758f2d74b8812afaf84
Odunayo Obajuluwa <odunayoobajuluwa@Odunayos-MacBook-Pro.local>
2017-06-08 16:31:35 -0500

    Project 1 with JS function working separately, but not together, leaner too.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,44 @@
  
-   // $("#shuffleBtn").click(function(){
-    // 	$(".cardBackWrapper").css("display", "block");
-    // 	$(".playingCard").css("display", "none");
-    // 	for (var i = 0; i < 10; i++) {
-    // 		var randomNum = Math.floor((Math.random()*2) + 1);
-    // 		if (randomNum === 1) {
-    // 			switchLeftCards();
-    // 		} else if (randomNum === 2) {
-    // 			switchRightCards();
-    // 		}
-    // 	}
-    // });
+   $("#shuffleBtn").click(function(){
+    	$(".cardBackWrapper").css("display", "block");
+    	$(".playingCard").css("display", "none");
+    	for (var i = 0; i < 10; i++) {
+    		switchLeftCards();
+    		var randomNum = Math.floor((Math.random()*2) + 1);
+    		if (randomNum === 1) {
+    			console.log("Switch Left Cards")
+    			switchLeftCards();
+    		} else if (randomNum === 2) {
+    			console.log("Switch Right Cards")
+    			switchRightCards();
+    		}
+    	}
+    });
 
 var switchLeftCards = function() {
-	var cards = []
 	var card1 = $("#back1 .cardBack");
 	var card2 = $("#back2 .cardBack");
 	card1.animate({left: '205px'});
 	card2.animate({left: '-205px'}, function(){
-		$("#back2").append(cards[0]);
-		$(cards[0]).css("left", "0");
-		$("#back1").append(cards[1]);
-		$(cards[1]).css("left", "0");
+		$("#back2").append(card1);
+		$(card1).css("left", "0");
+		$("#back1").append(card2);
+		$(card2).css("left", "0");
+		shuffle()
 	});
-	cards.push(card1)
-	cards.push(card2)
 }
 
 var switchRightCards = function() {
-	var cards = []
 	var card2 = $("#back2 .cardBack");
 	var card3 = $("#back3 .cardBack");
 	card2.animate({left: '205px'});
 	card3.animate({left: '-205px'}, function() {
-		$("#back3").append(cards[0]);
-		$(cards[0]).css("left", "0");
-		$("#back2").append(cards[1]);
-		$(cards[1]).css("left", "0");
+		$("#back3").append(card2);
+		$(card2).css("left", "0");
+		$("#back2").append(card3);
+		$(card3).css("left", "0");
+		shuffle()
 	});
-	cards.push(card2)
-	cards.push(card3)
 }
 
 $("#shuffleBtn").click(function(){
