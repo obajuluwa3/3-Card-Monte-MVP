f41fee12bdbfeb0b17108139f8c53542515598e3
Odunayo Obajuluwa <odunayoobajuluwa@Odunayos-MacBook-Pro.local>
2017-06-09 10:37:36 -0500

    Card fronts shuffling with backs

--- js/main.js
+++ js/main.js
@@ -24,13 +24,16 @@ var shuffle = function() {
 var switchLeftCards = function() {
 	var card1 = $("#back1 .cardBack");
 	var card2 = $("#back2 .cardBack");
+	var front1 = $("#card1Wrapper .playingCard")
+	var front2 = $("#card2Wrapper .playingCard")
 	card1.animate({left: '205px'}, function() {
-		// shuffle()
 	});
 	card2.animate({left: '-205px'}, function(){
 		$("#back2").append(card1);
+		$("#card2Wrapper").append(front1);
 		$(card1).css("left", "0");
 		$("#back1").append(card2);
+		$("#card1Wrapper").append(front2);
 		$(card2).css("left", "0");
 	});
 }
@@ -38,14 +41,17 @@ var switchLeftCards = function() {
 var switchRightCards = function() {
 	var card2 = $("#back2 .cardBack");
 	var card3 = $("#back3 .cardBack");
+	var front2 = $("#card2Wrapper .playingCard")
+	var front3 = $("#card3Wrapper .playingCard")
 	card2.animate({left: '205px'}, function() {
 	});
 	card3.animate({left: '-205px'}, function() {
 		$("#back3").append(card2);
+		$("#card3Wrapper").append(front2);
 		$(card2).css("left", "0");
 		$("#back2").append(card3);
+		$("#card2Wrapper").append(front3);
 		$(card3).css("left", "0");
-		// shuffle()
 	});
 }
 
