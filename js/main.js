aa64e1a3f487f7f181de0f444d47102f347785f4
Odunayo Obajuluwa <odunayoobajuluwa@Odunayos-MacBook-Pro.local>
2017-06-09 09:51:50 -0500

    Working shuffle module with setInterval function

--- js/main.js
+++ js/main.js
@@ -1,49 +1,50 @@
  
-   $("#shuffleBtn").click(function(){
-    	$(".cardBackWrapper").css("display", "block");
-    	$(".playingCard").css("display", "none");
-    	for (var i = 0; i < 10; i++) {
-    		switchLeftCards();
-    		var randomNum = Math.floor((Math.random()*2) + 1);
-    		if (randomNum === 1) {
-    			console.log("Switch Left Cards")
-    			switchLeftCards();
-    		} else if (randomNum === 2) {
-    			console.log("Switch Right Cards")
-    			switchRightCards();
-    		}
-    	}
-    });
+var shuffle = function() {
+	var randomNum = Math.floor((Math.random()*2) + 1);
+	if (randomNum === 1) {
+		console.log(randomNum)
+		console.log("Switch Left Cards")
+		switchLeftCards();
+	} else if (randomNum === 2) {
+		console.log(randomNum)
+		console.log("Switch Right Cards")
+		switchRightCards();
+	}
+}
+
 
 var switchLeftCards = function() {
 	var card1 = $("#back1 .cardBack");
 	var card2 = $("#back2 .cardBack");
-	card1.animate({left: '205px'});
+	card1.animate({left: '205px'}, function() {
+		// shuffle()
+	});
 	card2.animate({left: '-205px'}, function(){
 		$("#back2").append(card1);
 		$(card1).css("left", "0");
 		$("#back1").append(card2);
 		$(card2).css("left", "0");
-		shuffle()
 	});
 }
 
 var switchRightCards = function() {
 	var card2 = $("#back2 .cardBack");
 	var card3 = $("#back3 .cardBack");
-	card2.animate({left: '205px'});
+	card2.animate({left: '205px'}, function() {
+	});
 	card3.animate({left: '-205px'}, function() {
 		$("#back3").append(card2);
 		$(card2).css("left", "0");
 		$("#back2").append(card3);
 		$(card3).css("left", "0");
-		shuffle()
+		// shuffle()
 	});
 }
 
 $("#shuffleBtn").click(function(){
     $(".cardBackWrapper").css("display", "block");
     $(".playingCard").css("display", "none");
+    setInterval(function(){shuffle()}, 1000)
 	// switchLeftCards();
 	// switchRightCards();
 });
